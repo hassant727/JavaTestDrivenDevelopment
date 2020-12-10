9466327816e09a3beebb033a5cd809af8c4d4c87
ahmadt312 <32881885+ahmadt312@users.noreply.github.com>
2020-12-10 15:44:24 +0000

    inital commit

---
+++ Assignment3/src/test/java/com/colours/TestColour.java
@@ -0,0 +1,17 @@
+package com.colours;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+
+public class TestColour {
+
+    String x = "hello";
+
+    @Test
+    public void stringTest(){
+        //fail("failed");
+        System.out.println("pass");
+
+    }
+}
