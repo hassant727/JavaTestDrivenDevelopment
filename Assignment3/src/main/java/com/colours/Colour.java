e77df5daa6732d7ef5d9299127135c561cdb33b5
ahmadt312 <32881885+ahmadt312@users.noreply.github.com>
2020-12-13 13:09:02 +0000

    getComponent_one test class

---
+++ Assignment3/src/main/java/com/colours/Colour.java
@@ -0,0 +1,73 @@
+package com.colours;
+
+
+public class Colour {
+
+    public int component_one;
+    public int component_two;
+    public int component_three;
+    public String colourModel;
+
+    public Colour(int component_one, int component_two, int component_three, String colourModel) {
+
+        this.component_one = component_one;
+        this.component_two = component_two;
+        this.component_three = component_three;
+        this.colourModel = colourModel;
+    }
+
+    public Colour(int component_one, int component_two, int component_three) {
+        this.component_one = component_one;
+        this.component_two = component_two;
+        this.component_three = component_three;
+        this.colourModel = "RGB";
+
+    }
+
+    public int getComponent_one(){
+
+        return component_one;
+    }
+
+    public void setComponent_one(int col1){
+
+        this.component_one = col1;
+    }
+
+    public int getComponent_two(){
+
+        return component_two;
+    }
+
+    public void setComponent_two(int col2){
+
+        this.component_two = col2;
+    }
+
+    public int getComponent_three(){
+
+        return component_three;
+    }
+
+    public void setComponent_three(int col3){
+
+        this.component_three = col3;
+    }
+
+
+
+    void compare(){
+
+    }
+
+    void equals(){
+    }
+
+    void add(){
+
+    }
+
+
+
+
+}
---
+++ Assignment3/src/test/java/com/colours/TestColour.java
@@ -0,0 +1,54 @@
+package com.colours;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestColour{
+
+
+    @Test
+    public void getComponent_one() {
+        Colour one = new Colour(255, 255, 255);
+        assertEquals(255, one.getComponent_one());
+    }
+
+    @Test
+    public void setComponent_one() {
+    }
+
+    @Test
+    public void getComponent_two() {
+    }
+
+    @Test
+    public void setComponent_two() {
+    }
+
+    @Test
+    public void getComponent_three() {
+    }
+
+    @Test
+    public void setComponent_three() {
+    }
+
+    @Test
+    public void compare() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+
+
+
+
+
+
+}
