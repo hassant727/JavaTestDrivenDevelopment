be962333d9b39e5db3e3657f3984bead4abe19b7
ahmadt312 <32881885+ahmadt312@users.noreply.github.com>
2020-12-17 12:16:24 +0000

    initial commit

--- Assignment3/src/test/java/com/colours/TestColour.java
+++ Assignment3/src/test/java/com/colours/TestColour.java
@@ -7,55 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestColour{
 
 
-    @Test
-    public void getComponent_one() {
-        Colour one = new Colour(255, 255, 255);
-        assertEquals(255, one.getComponent_one());
-    }
-
-    @Test
-    public void setComponent_one() {
-        Colour one = new Colour(255, 255, 255, "RGB");
-        assertEquals(100, one.setComponent_one(100));
-
-    }
-
-    @Test
-    public void getComponent_two() {
-        Colour one = new Colour(255, 255, 255);
-        assertEquals(255, one.getComponent_two());
-    }
-
-    @Test
-    public void setComponent_two() {
-        Colour one = new Colour(255, 255, 255, "RGB");
-        assertEquals(100, one.setComponent_two(100));
-    }
-
-    @Test
-    public void getComponent_three() {
-        Colour one = new Colour(255, 255, 255);
-        assertEquals(255, one.getComponent_three());
-    }
-
-    @Test
-    public void setComponent_three() {
-        Colour one = new Colour(255, 255, 255, "RGB");
-        assertEquals(-100, one.setComponent_three(-100));
-    }
-
-    @Test
-    public void compare() {
-    }
-
-    @Test
-    public void testEquals() {
-    }
-
-    @Test
-    public void add() {
-    }
-
+   
 
 
 
