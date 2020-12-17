eb470eff65f71f3f97783ae3af0e210ec50138fa
ahmadt312 <32881885+ahmadt312@users.noreply.github.com>
2020-12-17 12:23:27 +0000

    initial fail case for class not defined

--- Assignment3/src/test/java/com/colours/TestColour.java
+++ Assignment3/src/test/java/com/colours/TestColour.java
@@ -7,7 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestColour{
 
 
-   
+    @Test
+    public void initialErrorColourClassNotDefined() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Colour colourObj = new Colour(255, 255, 255);
+        });
+    }
+
+
 
 
 
