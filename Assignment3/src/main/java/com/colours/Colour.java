8a7c972a290d51b8333ea44b6d2e95bb87843299
Hassan Tariq <118364536@umail.ucc.ie>
2020-12-17 13:14:24 +0000

    added checkingIfValuesOfTwoObjectsAreEqual() fail method

--- Assignment3/src/main/java/com/colours/Colour.java
+++ Assignment3/src/main/java/com/colours/Colour.java
@@ -1,3 +1,21 @@
 package com.colours;
 
 
+public class Colour{
+
+//    private int component_one;
+//    private int component_two;
+//    private int component_three;
+//    private String colourModel;
+//
+//    public Colour(int component_one, int component_two, int component_three){
+//        if (component_one > 255 || component_one < 0 || component_two > 255 || component_two < 0 || component_three > 255 || component_three <0){
+//            throw new IllegalArgumentException ("Values out of range");
+//        }
+//        this.component_one = component_one;
+//        this.component_two = component_two;
+//        this.component_three = component_three;
+//        this.colourModel = "RGB";
+//    }
+
+}
--- Assignment3/src/test/java/com/colours/TestColour.java
+++ Assignment3/src/test/java/com/colours/TestColour.java
@@ -4,21 +4,29 @@ import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestColour{
-
+public class TestColour {
 
     @Test
-    public void initialErrorColourClassNotDefined() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            Colour colourObj = new Colour(255, 255, 255);
-        });
+    public void initialFailTestCase() {
+
+//        System.out.println("hello");
+        fail("no implementation");
     }
 
+    @Test
+    public void checkingIfValuesOfTwoObjectsAreEqual(){
+        int r = 0, g = 0, b = 0;
+        int r1, g1, b1;
 
+        r1 = 100;
+        g1 = 150;
+        b1 = 255;
 
-
-
-
-
+        assertAll(
+                ()-> assertEquals(r, r1),
+                ()-> assertEquals(g, g1),
+                ()-> assertEquals(b, b1)
+        );
+    }
 
 }
